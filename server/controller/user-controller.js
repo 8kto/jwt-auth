4306c66cd9d38f1141f272802a9bea17fba70984
Okto <web@axisful.info>
2022-03-27 21:40:03 +0200

    Add request validation for POST req

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,9 +1,19 @@
+const { validationResult } = require('express-validator')
+
 const userService = require('../service/user-service')
 const UserModel = require('../models/user-model')
+const ApiError = require('../exceptions/api-error')
 
 class UserController {
   async registration (req, res, next) {
     try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return next(
+          ApiError.BadRequest('Validation errors', errors.array()),
+        )
+      }
+
       const { email, password } = req.body
       const userData = await userService.registration(email, password)
       res.cookie('refreshToken', userData.refreshToken, {
--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,16 @@
 const Router = require('express').Router
+const { body } = require('express-validator')
+
 const userController = require('../controller/user-controller')
 
 const router = new Router()
 
-router.post('/registration', userController.registration)
+router.post(
+  '/registration',
+  body('email').isEmail(),
+  body('password').isLength({ min: 3, max: 32 }),
+  userController.registration,
+)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
